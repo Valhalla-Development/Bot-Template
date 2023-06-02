c4317b6af9dd6afb5ddcbfe423ce789891d95b0d
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-06-02 16:13:34 +0100

    Create Util.ts

---
+++ src/utils/Util.ts
@@ -0,0 +1,56 @@
+import type { Message } from 'discord.js';
+import { Client } from 'discordx';
+import 'colors';
+
+/**
+ * Capitalises the first letter of each word in a string.
+ * @param string - The string to be capitalised.
+ * @returns The capitalised string.
+ */
+export function capitalise(string: string) {
+    return string.replace(/\S+/g, (word) => word.slice(0, 1).toUpperCase() + word.slice(1));
+}
+
+/**
+ * Checks if a message is deletable, and deletes it after a specified amount of time.
+ * @param message - The message to check.
+ * @param time - The amount of time to wait before deleting the message, in milliseconds.
+ * @returns void
+ */
+export function deletableCheck(message: Message, time: number): void {
+    setTimeout(() => {
+        if (message && message.deletable) {
+            message.delete().catch(console.error);
+        }
+    }, time);
+}
+
+/**
+ * Fetches the registered global application commands and returns an object
+ * containing the command names as keys and their corresponding IDs as values.
+ * @param client - The Discord Client instance.
+ * @returns An object containing command names and their corresponding IDs.
+ * If there are no commands or an error occurs, an empty object is returned.
+ */
+export async function getCommandIds(client: Client) {
+    try {
+        // Fetch the registered global application commands
+        const commands = await client.application?.commands.fetch();
+
+        if (!commands) {
+            return {};
+        }
+
+        // Create an object to store the command IDs
+        const commandIds: { [name: string]: string } = {};
+
+        commands.forEach((command) => {
+            commandIds[command.name] = command.id;
+        });
+
+        return commandIds;
+    } catch (error) {
+        console.error('Error fetching global commands:', error);
+        return {};
+    }
+}
