533af7754987b4206d4c508bbfe3dc3affeffedb
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2024-10-21 21:11:24 +0100

    📝 (Main.ts): refactor error handling to use a centralized handleError function for unhandled rejections and uncaught exceptions 🔧 (Main.ts): update environment variable names for better clarity and add validation for required variables ♻️ (Main.ts): refactor code to use arrow functions for concise error messages and improve error handling logic

--- src/Main.ts
+++ src/Main.ts
@@ -1,9 +1,8 @@
 import { dirname, importx } from '@discordx/importer';
-import {
-    ChannelType, codeBlock, EmbedBuilder, IntentsBitField,
-} from 'discord.js';
+import { IntentsBitField } from 'discord.js';
 import { Client } from 'discordx';
 import 'dotenv/config';
+import { handleError } from './utils/Util.js';
 
 /**
  * The Discord.js client instance.
@@ -19,37 +18,16 @@ const client = new Client({
  * @returns void
  */
 process.on('unhandledRejection', async (error) => {
-    if (!error || !(error instanceof Error) || !error.stack) return;
-    console.error(error.stack);
+    await handleError(client, error);
+});
 
-    if (process.env.Logging && process.env.Logging.toLowerCase() === 'true') {
-        if (!process.env.LoggingChannel) return;
-
-        const channel = client.channels.cache.get(process.env.LoggingChannel);
-        if (!channel || channel.type !== ChannelType.GuildText) return;
-
-        const typeOfError = error.stack.split(':')[0];
-        const fullError = error.stack.replace(/^[^:]+:/, '').trimStart();
-        const timeOfError = `<t:${Math.floor(new Date().getTime() / 1000)}>`;
-        const fullString = `From: \`${typeOfError}\`\nTime: ${timeOfError}\n\nError:\n${codeBlock('js', fullError)}`;
-
-        function truncateDescription(description: string) {
-            const maxLength = 2048;
-            if (description.length > maxLength) {
-                const numTruncatedChars = description.length - maxLength;
-                return `${description.slice(0, maxLength)}... ${numTruncatedChars} more`;
-            }
-            return description;
-        }
-
-        const embed = new EmbedBuilder().setTitle('Error').setDescription(truncateDescription(fullString));
-
-        try {
-            await channel.send({ embeds: [embed] });
-        } catch (sendError) {
-            console.error('An error occurred while sending the error embed:', sendError);
-        }
-    }
+/**
+ * Handles uncaught exception by logging the error and sending an embed to a designated logging channel, if enabled.
+ * @param error - The error that was not handled.
+ * @returns void
+ */
+process.on('uncaughtException', async (error) => {
+    await handleError(client, error);
 });
 
 /**
@@ -59,13 +37,15 @@ process.on('unhandledRejection', async (error) => {
  * @throws An Error if any required environment variables are missing or invalid.
  */
 async function run() {
-    const missingTokenError = 'The Token environment variable is missing.';
-    const invalidLoggingValueError = 'The Logging environment variable must be "true" or "false".';
-    const invalidLoggingChannel = 'The LoggingChannel environment variable is required when logging is enabled.';
+    const missingVar = (v: string) => `The ${v} environment variable is missing.`;
 
-    if (!process.env.Token) throw new Error(missingTokenError);
-    if (process.env.Logging === 'true' && !process.env.LoggingChannel) throw new Error(invalidLoggingChannel);
-    if (process.env.Logging !== 'true' && process.env.Logging !== 'false') throw new Error(invalidLoggingValueError);
+    const required = ['BOT_TOKEN'];
+
+    required.forEach((v) => { if (!process.env[v]) throw new Error(missingVar(v)); });
+
+    if (process.env.ENABLE_LOGGING?.toLowerCase() === 'true' && (!process.env.ERROR_LOGGING_CHANNEL && !process.env.COMMAND_LOGGING_CHANNEL)) {
+        throw new Error('ERROR_LOGGING_CHANNEL and COMMAND_LOGGING_CHANNEL are required when logging is enabled.');
+    }
 
     /**
      * Delays the execution of the function for a specified time in milliseconds.
@@ -85,7 +65,7 @@ async function run() {
         try {
             await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
             await sleep(time);
-            await client.login(process.env.Token as string);
+            await client.login(process.env.BOT_TOKEN as string);
         } catch (error) {
             console.error('An error occurred while initializing the bot:', error);
         }
